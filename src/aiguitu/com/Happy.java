826df9bfea8961e8b0ff5a39a4f43632bec93017
tom_glb <goodMorning_glb@atguigu.com>
2019-08-05 11:59:07 +0800

    新增

--- src/aiguitu/com/Happy.java
+++ src/aiguitu/com/Happy.java
@@ -3,5 +3,10 @@ package aiguitu.com;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("TestGit2");
+		
+		int sum = 0;
+		int a = 1;
+		int b = 2;
+		sum = a + b;
 	}
 }
